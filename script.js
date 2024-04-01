c5cdd0135f564a6e20fa911558540f5f361b1b8d
Lavina <150042947+lavinad04@users.noreply.github.com>
2024-04-01 12:21:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,23 +5,20 @@ constructor(name, age) {
 	this.age = age;
 }
 	greet(){
-		console.log(`Hello, my name is${this.name}", I am${this.age} years old`);
+		console.log(`Hello, my name is ${this.name}", I am${this.age} years old.`);
 	}
 }
 
 class Employee extends Person{
 	constructor(name, age, jobTitle) {
-		super(name,age);
+		super(name, age);
 	this.jobTitle = jobTitle;
 	}
 	jobGreet(){
-		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`);
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 	}
 }
 
-// Do not change code below this line
-// const car = new SportsCar("Ferrari", "Testarossa", 200);
-// const two = new Car("suite", "bmw");
 let alice = new Person("Alice", 25);
 let bob = new Employee("Bob", 30, "Manager");
 bob.jobGreet();
